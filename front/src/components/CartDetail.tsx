a563d4b0521f36d0f39cff5e5cf2e5c16e2fc0a3
EmilianoAllende <emilianoallende94@gmail.com>
2025-05-25 15:29:09 -0300

    Dashboard is no longer "use client".

--- front/src/components/CartDetail.tsx
+++ front/src/components/CartDetail.tsx
@@ -35,7 +35,7 @@ const CartDetail = () => {
     };
 
     return (
-        <div className="max-w-4xl mx-auto p-4 bg-primaryColor shadow-lg rounded-lg">
+        <div className="max-w-4xl mx-auto p-4 bg-primaryColor shadow-lg rounded-lg mt-8">
             {cart.length === 0 ? (
                 <div className="flex flex-col items-center text-center">
                     <h2 className="bg-tertiaryColor text-3xl text-center font-bold rounded-xl px-3">CART IS EMPTY.</h2>
